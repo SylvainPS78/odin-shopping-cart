38094f875f41101da0769b7e00edf66ca017d124
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-08-30 08:49:13 +0700

    FEAT Change color constants and fonts

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,37 +3,31 @@
 // ==============================================
 
 // Colors - Primary Palette
-export const PRIMARY_BLUE = "#5367ff";
-export const LIGHT_PRIMARY_BLUE = "#7f8dfc";
+export const DEEP_BLUE = "#234e70";
+export const GOLD = "#f7c873";
 
 // Colors - Accent Palette
-export const ACCENT_RED = "#fa5757";
-export const LIGHT_ACCENT_RED = "#ff8787";
+export const ACCENT_RED = "#e94560";
 
 // Colors - Neutral Palette
 export const WHITE = "#ffffff";
 export const BLACK = "#000000";
-export const LIGHT_GREY = "#f0f0f0";
-export const MEDIUM_GREY = "#9194a2";
-export const DARK_GREY = "#242940";
+export const LIGHT_GREY = "#f2f2f2";
 
 // Export as object for easier usage
 export const COLORS = {
-  PRIMARY_BLUE,
-  LIGHT_PRIMARY_BLUE,
+  DEEP_BLUE,
+  GOLD,
   ACCENT_RED,
-  LIGHT_ACCENT_RED,
+
   WHITE,
   BLACK,
   LIGHT_GREY,
-  MEDIUM_GREY,
-  DARK_GREY,
 };
 
 // Default colors for specific contexts
 export const ICON_COLORS = {
-  DEFAULT: DARK_GREY,
-  DELETE: LIGHT_ACCENT_RED,
-  PRIMARY: PRIMARY_BLUE,
-  SECONDARY: MEDIUM_GREY,
+  DEFAULT: DEEP_BLUE,
+  DELETE: ACCENT_RED,
+  SECONDARY: GOLD,
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+//import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
 
