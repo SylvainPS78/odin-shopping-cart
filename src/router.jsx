f4ddab484a9569d5c57717809182de375608e4e5
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-08-30 15:13:32 +0700

    FEAT Seperate route logic in seperate file

---
+++ src/router.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import HomePage from "./assets/components/HomePage.jsx";
+import AboutPage from "./assets/components/AboutPage.jsx";
+// import ProductPage from "./assets/components/ProductPage.jsx";
+// import CartPage from "./assets/components/CartPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      /*{
+        path: "products",
+        element: <ProductPage />,
+      },
+      {
+        path: "cart",
+        element: <CartPage />,
+      },*/
+    ],
+  },
+]);
+
+export default router;
