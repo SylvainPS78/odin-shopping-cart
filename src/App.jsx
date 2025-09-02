2ddac92b3ff121fb44638651ae32555945146aa6
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-09-02 14:35:48 +0700

    FEAT Create CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import "./assets/styles/App.css";
 import Header from "./assets/components/Header.jsx";
+import { useState } from "react";
+import { CartContext } from "./contexts/CartContext.js";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
   return (
     <>
-      <Header />
-      <Outlet />
+      <CartContext.Provider value={{ cartItems, setCartItems }}>
+        <Header />
+        <Outlet />
+      </CartContext.Provider>
     </>
   );
 }
